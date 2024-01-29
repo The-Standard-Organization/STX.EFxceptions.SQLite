8b7170c18364224771af483342ee1c579ff56fe0
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2024-01-29 21:06:10 +0900

    BROKERS: Get Status Code

---
+++ STX.EFxceptions.SQLite.Base/Brokers/DbErrorBroker/SQLiteErrorBroker.cs
@@ -0,0 +1,14 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Data.Sqlite;
+
+namespace STX.EFxceptions.SQLite.Base.Brokers.DbErrorBroker
+{
+    public class SQLiteErrorBroker : ISQLiteErrorBroker
+    {
+        public int GetErrorCode(SqliteException sqlitException) =>
+            sqlitException.ErrorCode;
+    }
+}
