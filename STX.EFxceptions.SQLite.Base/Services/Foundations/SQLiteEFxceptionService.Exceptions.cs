de4581914091b20726c6a8d3385cf121c61b3c80
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2024-01-29 23:18:11 +0900

    ShouldThrowInvalidObjectNameSqliteException -> PASS

--- STX.EFxceptions.SQLite.Base/Services/Foundations/SQLiteEFxceptionService.Exceptions.cs
+++ STX.EFxceptions.SQLite.Base/Services/Foundations/SQLiteEFxceptionService.Exceptions.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using STX.EFxceptions.SQLite.Base.Models.Exceptions;
+﻿using STX.EFxceptions.SQLite.Base.Models.Exceptions;
 
 namespace STX.EFxceptions.SQLite.Base.Services.Foundations
 {
@@ -11,10 +6,12 @@ namespace STX.EFxceptions.SQLite.Base.Services.Foundations
     {
         private void ConvertAndThrowMeaningfulException(int sqliteErrorCode, string message)
         {
-            switch(sqliteErrorCode)
+            switch (sqliteErrorCode)
             {
                 case 207:
                     throw new InvalidColumnNameException(message);
+                case 208:
+                    throw new InvalidObjectNameSqliteException(message);
             }
         }
     }
