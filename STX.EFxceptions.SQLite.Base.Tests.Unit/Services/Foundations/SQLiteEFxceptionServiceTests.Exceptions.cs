eaf25b9fe451e7dc85a31f20bcb0de5c6c1027a7
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2024-01-29 22:40:28 +0900

    ShouldThrowDbUpdateExceptionIfErrorCodeIsNotRecognized -> FAIL

---
+++ STX.EFxceptions.SQLite.Base.Tests.Unit/Services/Foundations/SQLiteEFxceptionServiceTests.Exceptions.cs
@@ -0,0 +1,34 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace STX.EFxceptions.SQLite.Base.Tests.Unit.Services.Foundations
+{
+    public partial class SQLiteEFxceptionServiceTests
+    {
+        [Fact]
+        public void ShouldThrowDbUpdateExceptionIfErrorCodeIsNotRecognized()
+        {
+            // given
+            int sqlForeignKeyConstraintConflictErrorCode = 0000;
+            string randomErrorMessage = CreateRandomErrorMessage();
+            SqliteException foreignKeyConstraintConflictException = CreateSQLiteException();
+
+            var dbUpdateException = new DbUpdateException(
+                message: randomErrorMessage,
+                innerException: foreignKeyConstraintConflictException);
+
+            this.sqliteErrorBrokerMock.Setup(broker =>
+                broker.GetErrorCode(foreignKeyConstraintConflictException))
+                    .Returns(sqlForeignKeyConstraintConflictErrorCode);
+
+            // when . then
+            Assert.Throws<DbUpdateException>(() =>
+                this.sqliteEFxceptionService.ThrowMeaningfulException(dbUpdateException));
+        }
+    }
+}
--- STX.EFxceptions.SQLite.Base.Tests.Unit/Services/Foundations/SQLiteEFxceptionServiceTests.cs
+++ STX.EFxceptions.SQLite.Base.Tests.Unit/Services/Foundations/SQLiteEFxceptionServiceTests.cs
@@ -2,9 +2,12 @@
 // Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System.Runtime.Serialization;
+using Microsoft.Data.Sqlite;
 using Moq;
 using STX.EFxceptions.SQLite.Base.Brokers.DbErrorBroker;
 using STX.EFxceptions.SQLite.Base.Services.Foundations;
+using Tynamix.ObjectFiller;
 
 namespace STX.EFxceptions.SQLite.Base.Tests.Unit.Services.Foundations
 {
@@ -20,5 +23,10 @@ namespace STX.EFxceptions.SQLite.Base.Tests.Unit.Services.Foundations
             this.sqliteEFxceptionService = new SQLiteEFxceptionService(
                 sqliteErrorBroker: this.sqliteErrorBrokerMock.Object);
         }
+
+        private string CreateRandomErrorMessage() => new MnemonicString().GetValue();
+
+        private SqliteException CreateSQLiteException() =>
+            FormatterServices.GetSafeUninitializedObject(typeof(SqliteException)) as SqliteException;
     }
 }
