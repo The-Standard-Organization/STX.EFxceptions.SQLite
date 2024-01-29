7df27c0f7ea7a1c15a2513d3c83821c19310994a
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2024-01-29 22:18:21 +0900

    ShouldThrowDbUpdateExceptionIfSQLiteExceptionsIsNull -> FAIL

---
+++ STX.EFxceptions.SQLite.Base.Tests.Unit/Services/Foundations/SQLiteEFxceptionServiceTests.cs
@@ -0,0 +1,24 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Moq;
+using STX.EFxceptions.SQLite.Base.Brokers.DbErrorBroker;
+using STX.EFxceptions.SQLite.Base.Services.Foundations;
+
+namespace STX.EFxceptions.SQLite.Base.Tests.Unit.Services.Foundations
+{
+    public partial class SQLiteEFxceptionServiceTests
+    {
+        private readonly Mock<ISQLiteErrorBroker> sqliteErrorBrokerMock;
+        private readonly ISQLiteEFxceptionService sqliteEFxceptionService;
+
+        public SQLiteEFxceptionServiceTests()
+        {
+            this.sqliteErrorBrokerMock = new Mock<ISQLiteErrorBroker>();
+
+            this.sqliteEFxceptionService = new SQLiteEFxceptionService(
+                sqliteErrorBroker: this.sqliteErrorBrokerMock.Object);
+        }
+    }
+}
---
+++ STX.EFxceptions.SQLite.Base/Services/Foundations/SQLiteEFxceptionService.cs
@@ -0,0 +1,23 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using STX.EFxceptions.Interfaces.Brokers.DbErrorBroker;
+
+namespace STX.EFxceptions.SQLite.Base.Services.Foundations
+{
+    public class SQLiteEFxceptionService : ISQLiteEFxceptionService
+    {
+        private readonly IDbErrorBroker<SqliteException> sqliteErrorBroker;
+
+        public SQLiteEFxceptionService(IDbErrorBroker<SqliteException> sqliteErrorBroker) =>
+            this.sqliteErrorBroker = sqliteErrorBroker;
+
+        public void ThrowMeaningfulException(DbUpdateException dbUpdateException)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
