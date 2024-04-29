868fcf8254eb429ac72e66a130a20b455afc0d55
ZafarUrakov <goliburakov535@gmail.com>
2024-04-29 16:36:55 +0500

    CODE RUB: Change Exception -> DbUpdateException

--- STX.EFxceptions.SQLite.Base/Models/Exceptions/DuplicateKeyWithUniqueIndexSQLiteException.cs
+++ STX.EFxceptions.SQLite.Base/Models/Exceptions/DuplicateKeyWithUniqueIndexSQLiteException.cs
@@ -2,11 +2,11 @@
 // Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace STX.EFxceptions.SQLite.Base.Models.Exceptions
 {
-    public class DuplicateKeyWithUniqueIndexSQLiteException : Exception
+    public class DuplicateKeyWithUniqueIndexSQLiteException : DbUpdateException
     {
         public DuplicateKeyWithUniqueIndexSQLiteException(string message) : base(message) { }
     }
--- STX.EFxceptions.SQLite.Base/Models/Exceptions/InvalidObjectNameSQLiteException.cs
+++ STX.EFxceptions.SQLite.Base/Models/Exceptions/InvalidObjectNameSQLiteException.cs
@@ -2,15 +2,11 @@
 // Copyright(c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace STX.EFxceptions.SQLite.Base.Models.Exceptions
 {
-    public class InvalidObjectNameSQLiteException : Exception
+    public class InvalidObjectNameSQLiteException : DbUpdateException
     {
         public InvalidObjectNameSQLiteException(string message) : base(message) { }
     }
